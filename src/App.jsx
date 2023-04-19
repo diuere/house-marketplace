667031db846ae0323b19f1fbca4f59823870fbed
diuere <86838002+diuere@users.noreply.github.com>
2023-04-19 19:16:45 -0300

    app initialized with CRA

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Router, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Router>
+        <Route />
+      </Router>
+    </BrowserRouter>
+  );
+}
+
+export default App;
