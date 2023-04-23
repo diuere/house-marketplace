55bf9f8cd983f4e63985adc679cc18cab581bfb5
diuere <86838002+diuere@users.noreply.github.com>
2023-04-23 20:32:18 -0300

    Listing and Contact page added

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import Profile from "./pages/Profile";
 import PrivateRoute from "./components/PrivateRoute";
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -26,6 +28,8 @@ function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/create-listing" element={<CreateListing />} />
+        <Route path="/category/:categoryName/:listingId" element={<Listing />} />
+        <Route path="/contact/:landlordId" element={<Contact />} />
       </Routes>
       <Navbar />
       <ToastContainer autoClose={3000}/>
---
+++ src/pages/Listing.jsx
@@ -0,0 +1,97 @@
+import { useEffect, useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { Link, useParams } from "react-router-dom";
+import { getAuth } from "firebase/auth";
+import { db } from "../misc/firebase";
+import Spinner from "../components/Spinner";
+import shareIcon from "../assets/svg/shareIcon.svg";
+
+const Listing = () => {
+  const [listing, setListing] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [sharedLinkCopied, setSharedLinkCopied] = useState(null);
+
+  const params = useParams();
+  const auth = getAuth();
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      const docRef = doc(db, "listings", params.listingId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setListing(docSnap.data());
+        setIsLoading(false);
+      }
+    };
+    fetchListing();
+  }, [params.listingId]);
+
+  const handleShareIconClick = () => {
+    navigator.clipboard.writeText(window.location.href);
+    setSharedLinkCopied(true);
+    setTimeout(() => {
+      setSharedLinkCopied(false);
+    }, 2000);
+  };
+
+  if (isLoading) return <Spinner />;
+
+  return (
+    <main>
+      <div className="shareIconDiv" onClick={handleShareIconClick}>
+        <img src={shareIcon} alt="share icon" />
+      </div>
+
+      {sharedLinkCopied && <p className="linkCopied">Link Copied!</p>}
+
+      <div className='listingDetails'>
+        <p className='listingName'>
+          {listing.name} - $
+          {listing.offer
+            ? listing.discountedPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+            : listing.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+        </p>
+        <p className='listingLocation'>{listing.address}</p>
+        <p className='listingType'>
+          For {listing.type === 'rent' ? 'Rent' : 'Sale'}
+        </p>
+        {listing.offer && (
+          <p className='discountPrice'>
+            ${listing.regularPrice - listing.discountedPrice} discount
+          </p>
+        )}
+
+        <ul className='listingDetailsList'>
+          <li>
+            {listing.bedrooms > 1
+              ? `${listing.bedrooms} Bedrooms`
+              : '1 Bedroom'}
+          </li>
+          <li>
+            {listing.bathrooms > 1
+              ? `${listing.bathrooms} Bathrooms`
+              : '1 Bathroom'}
+          </li>
+          <li>{listing.parking && 'Parking Spot'}</li>
+          <li>{listing.furnished && 'Furnished'}</li>
+        </ul>
+
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}`}
+            className='primaryButton'
+          >
+            Contact Landlord
+          </Link>
+        )}
+      </div>
+    </main>
+  );
+};
+
+export default Listing;
